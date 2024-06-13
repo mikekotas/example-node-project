e23894265ddc833bbe3c82c10b2115051feaddd6
Michalis Kotantakis <mike.kotantakis@gmail.com>
2024-06-13 20:44:20 +0200

    "Add new endpoint and test"

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,3 +8,11 @@ describe('GET /', () => {
     expect(response.text).toBe('Hello, world!');
   });
 });
+
+describe('GET /new', () => {
+    it('should respond with New endpoint', async () => {
+      const response = await request(app).get('/new');
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toBe('New endpoint');
+    });
+  });
