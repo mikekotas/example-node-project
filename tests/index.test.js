b1f3950477cdb2768362998459f31e4e6b8c9437
Michalis Kotantakis <mike.kotantakis@gmail.com>
2024-06-14 04:18:38 +0200

    upda

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,8 +5,7 @@ const http = require('http');
 let server;
 
 beforeAll((done) => {
-  server = http.createServer(app).listen(4000, done); // Start the server on port 4000
-  // This will start the server on port 4000
+  server = http.createServer(app).listen(4000, done); // Start the server on port 4000 and listen on port
 });
 
 afterAll((done) => {
